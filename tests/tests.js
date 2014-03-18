2f76efb09e34a8b849916ead13fe903016bb5bec
toulon <coderng@gmail.com>
2014-03-18 13:19:54 -0700

    Added Asychronous Tests

--- tests/tests.js
+++ tests/tests.js
@@ -8,8 +8,24 @@ var assert = require('assert');
 describe('Test Framework', function () {
   it('should have mocha installed and running.', function () {
     assert.equal(true, true);
-  })
+  });
   it('should have libray installed and running for fluent testing.', function () {
     true.should.eql(true);
+  })
+});
+
+describe('Asynchronous testing', function () {
+  var result = false;
+  beforeEach(function (done) {
+    setTimeout(function () {
+
+      result = true;
+      // complete the async beforeEach
+      done();
+    }, 2000);
+  });
+  it('should NOT be so hard so this makes it easier.', function (done) {
+    result.should.eql(true);
+    done();
   })
 });
